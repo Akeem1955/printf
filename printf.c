8576bcbc9f70e0c90dcc38ef818ce125e7804d0c
Akeem1955 <adetunjiadams29@gmail.com>
2022-11-21 05:47:06 -0800

    cisfun

---
+++ printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _printf - print out formated string
+ * @format: string to format
+ * Return: length of string
+ */
+int _printf(const char *format, ...)
+{
+	char c, *p;
+
+	int n, N;
+
+	va_list params;
+
+	N = 0;
+	va_start(params, format);
+	while (*format)
+	{
+		if (*(format) == '%')
+		{
+			switch (*(format + 1))
+			{
+			case 's':
+				p = va_arg(params, char*);
+				n = _strlen(p);
+				write(1, p, n);
+				format++;
+				break;
+			case 'c':
+				c = va_arg(params, int);
+				write(1, &c, 1);
+				format++;
+				break;
+			default:
+				break;
+			}
+		} else
+		{
+			write(1, format, 1);
+		}
+		format++;
+		N++;
+	}
+	va_end(params);
+	return (0);
+}
---
+++ utils.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<unistd.h>
+/**
+ * _strlen - return length of a string
+ * @str: string
+ * Return: length of string
+ */
+
+int _strlen(char *str)
+{
+	int n;
+
+	n = 0;
+	while (*str)
+	{
+		str++;
+		n++;
+	}
+	return (n);
+}
