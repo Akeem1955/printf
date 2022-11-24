b38de8b368a624d640940ac8d5481cb60a95fe39
Akeem1955 <adetunjiadams29@gmail.com>
2022-11-24 18:57:06 +0100

    cisfun

--- print_number.c
+++ print_number.c
@@ -1,30 +1,35 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
  * print_number - prints a number send to this function
  * @args: List of arguments
  * Return: The number of arguments printed
  */
-int print_num(va_list args)
+int print_number(va_list args)
 {
+	int n;
 	int div;
 	int len;
-	int num;
+	unsigned int num;
 
-	num  = va_arg(args, int);
+	n  = va_arg(args, int);
 	div = 1;
 	len = 0;
-	if (num < 0)
+
+	if (n < 0)
 	{
-		len += _putchar('-');
-		num = num * -1;
+		len += _write_char('-');
+		num = n * -1;
 	}
+	else
+		num = n;
 
 	for (; num / div > 9; )
 		div *= 10;
 
 	for (; div != 0; )
 	{
-		len += _putchar('0' + num / div);
+		len += _write_char('0' + num / div);
 		num %= div;
 		div /= 10;
 	}
@@ -36,22 +41,23 @@ int print_num(va_list args)
  * @n: unsigned integer to be printed
  * Return: The amount of numbers printed
  */
-int print_unum(va_list n)
+int print_unsgined_number(unsigned int n)
 {
 	int div;
 	int len;
 	unsigned int num;
 
-	num = va_arg(n, int); 
 	div = 1;
 	len = 0;
+
+	num = n;
 
 	for (; num / div > 9; )
 		div *= 10;
 
 	for (; div != 0; )
 	{
-		len += _putchar('0' + num / div);
+		len += _write_char('0' + num / div);
 		num %= div;
 		div /= 10;
 	}
